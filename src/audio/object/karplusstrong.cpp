a8565d082eab796515d0db6735661bba4e891852
Tim Groeneboom <tim@timgroeneboom.com>
2025-12-29 10:50:13 +0100

    - deleted a lot of stuff not needed by timecoder to speed up compilation

--- src/audio/object/karplusstrong.cpp
+++
@@ -1,9 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
-#include "karplusstrong.h"
-
-RTTI_DEFINE_CLASS(nap::audio::KarplusStrongObject)
-RTTI_DEFINE_CLASS(nap::audio::KarplusStrongInstance)
-
